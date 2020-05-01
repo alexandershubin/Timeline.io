4cfdc6baf71bdfbabd2e7c0d086656d599701f01
alexandershubin <shubin8@mail.ru>
2020-05-01 15:51:11 +0300

    fixed classes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
+'use strict'
 const button = document.querySelectorAll('.button');
 const timelineList = document.querySelector('.timeline__list');
+const timelineItem = document.querySelectorAll('.timeline__item');
 
 button.forEach(item => {
   item.addEventListener('click', (evt) => {
     const { target } = evt;
-    const buttonFirst = target.matches('.button__first');
-    const buttonSecond = target.matches('.button__second');
-    const buttonThird = target.matches('.button__third');
+    const buttonFirst = target.matches('.button__nav--top');
+    const buttonSecond = target.matches('.button__nav--bottom');
+    const buttonThird = target.matches('.button__nav--toggle');
+    const buttonFifth = target.matches('.button__nav--all');
     
     if (buttonFirst) {
-      timelineList.classList.add('timeline__list--top')
-      timelineList.classList.remove('timeline__list--bottom')
+      timelineList.classList.add('timeline__list--top');
+      timelineList.classList.remove('timeline__list--bottom');
     }
   
     if (buttonSecond) {
-      timelineList.classList.add('timeline__list--bottom')
-      timelineList.classList.remove('timeline__list--top')
+      timelineList.classList.add('timeline__list--bottom');
+      timelineList.classList.remove('timeline__list--top');
     }
     
     if (buttonThird) {
-      timelineList.classList.toggle('item-double')
+      timelineList.classList.toggle('item-double');
+    }
+    
+    if (buttonFifth) {
+      createComponent(timelineList)
     }
   })
 });
