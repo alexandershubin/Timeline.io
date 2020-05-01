f2a88be023c5cf41c735f317c38f9827a3dc6c36
alexandershubin <shubin8@mail.ru>
2020-05-01 23:13:26 +0300

    fixed name func in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const button = document.querySelectorAll('.button__nav');
 const timelineList = document.querySelector('.timeline__list');
 
-const deformationBlockList = (text) => {
+const createCloneBlock = (text) => {
   const timelineList2 = text.cloneNode(true)
   return text.after(timelineList2)
 }
@@ -12,7 +12,7 @@ button.forEach(item => {
     const {target} = e;
     const type = target.getAttribute('data-type');
     
-    if (!type) return deformationBlockList(timelineList);
+    if (!type) return createCloneBlock(timelineList);
     timelineList.className = 'timeline__list';
     timelineList.classList.add(`timeline__list--${type}`)
   });
